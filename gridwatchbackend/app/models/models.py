f0e762e961ec561d9e57cc9cfe668c120befdf95
Eshan Gillani <eshangillani1@gmail.com>
2026-03-07 13:59:21 -0500

    Updated models.py

--- gridwatchbackend/app/models/models.py
+++ gridwatchbackend/app/models/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
 
 base = declarative_base()
@@ -12,6 +13,6 @@ class User(base):
     email = Column(String)
     password = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
-    
+
     
     
