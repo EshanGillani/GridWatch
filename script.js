2b618b564c2da7b3a91a7cd504801a7a8874510b
Eshan Gillani <eshangillani1@gmail.com>
2026-03-08 03:06:15 -0400

    Updated predictions.html and script.js

--- script.js
+++ script.js
@@ -1,83 +1,39 @@
-const map = L.map('map').setView([38.9,-77.03],8);
+document.addEventListener("DOMContentLoaded", function () {
 
-L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{ attribution:'OSM'}
-).addTo(map);
+    const dropdown = document.getElementById("cityDropdown");
 
-let clusterGroup = L.markerClusterGroup();
-let heatPoints = [];
-let heatLayer = L.heatLayer([], {radius:25});
+    dropdown.addEventListener("change", function () {
+        updatePrediction();
+    });
 
-map.addLayer(clusterGroup);
-map.addLayer(heatLayer);
-
-async function loadOutages(){
-
-clusterGroup.clearLayers();
-heatPoints = [];
-
-const res = await fetch("data/outages.geojson");
-const data = await res.json();
-
-let countyTotals = {};
-
-data.features.forEach(feature=>{
-
-const coords = feature.geometry.coordinates;
-const props = feature.properties;
-
-const lat = coords[1];
-const lon = coords[0];
-
-let severityColor = "green";
-
-if(props.customers > 1000) severityColor = "red";
-else if(props.customers > 200) severityColor = "orange";
-
-const marker = L.circleMarker([lat,lon],{
-radius:8,
-color:severityColor
-});
-
-marker.bindPopup(
-`<b>${props.county}</b><br>
-Customers Out: ${props.customers}`
-);
-
-clusterGroup.addLayer(marker);
-
-heatPoints.push([lat,lon,props.customers/1000]);
-
-if(!countyTotals[props.county])
-countyTotals[props.county] = 0;
-
-countyTotals[props.county] += props.customers;
+    // run once on page load
+    updatePrediction();
 
 });
 
-heatLayer.setLatLngs(heatPoints);
 
-renderSidebar(countyTotals);
+function updatePrediction() {
+
+    const city = document.getElementById("cityDropdown").value;
+
+    const prediction = predictOutage(city);
+
+    document.getElementById("prediction").innerText =
+        "Predicted outages: " + prediction;
 
 }
 
-function renderSidebar(totals){
 
-let html = "";
+function predictOutage(city) {
 
-Object.keys(totals).forEach(c=>{
+    // Example prediction data
+    const outageData = {
+        "Fairfax": 120,
+        "Arlington": 75,
+        "Alexandria": 90,
+        "Reston": 60
+    };
 
-html += `<div class="county">
-${c}: ${totals[c]} customers
-</div>`;
-
-});
-
-document.getElementById("stats").innerHTML = html;
+    return outageData[city] || 0;
 
 }
-
-loadOutages();
-
-setInterval(loadOutages,300000);
