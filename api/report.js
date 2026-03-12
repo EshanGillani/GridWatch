f1b0b7669dd397de39e64d1f2db7400245c38115
eastmohan <eastmohan22@gmail.com>
2026-03-12 14:11:06 -0400

    updated reporting function

--- api/report.js
+++ api/report.js
@@ -1,56 +1,21 @@
-const OWNER = "EshanGillani";
-const REPO = "GridWatch";
-const PATH = "reports.json";
+export default async function hander(req,res){
+    if(req.method !== "POST"){
+        return res.status(405).json({error:"Method not allowed"})
+    }
 
-export default async function handler(req,res){
 
-if(req.method !== "POST"){
-return res.status(405).json({error:"Method not allowed"});
+const { zip, type, description } = req.body
+
+const report={
+    zip,
+    type,
+    description,
+    time:Date.now()
 }
 
-const {location,type,description} = req.body;
+global.reports = global.reports || []
 
-if(!location || !type){
-return res.status(400).json({error:"Missing data"});
-}
+global.reports.push(report)
 
-const token = process.env.GH_TOKEN;
-
-const headers = {
-Authorization:`Bearer ${token}`,
-Accept:"application/vnd.github.v3+json"
-};
-
-const file = await fetch(
-`https://api.github.com/repos/${OWNER}/${REPO}/contents/${PATH}`,
-{headers}
-);
-
-const data = await file.json();
-
-const json = JSON.parse(Buffer.from(data.content,"base64").toString());
-
-json.reports.push({
-location,
-type,
-description,
-reportedAt:new Date().toISOString()
-});
-
-await fetch(
-`https://api.github.com/repos/${OWNER}/${REPO}/contents/${PATH}`,
-{
-method:"PUT",
-headers:{
-...headers,
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-message:`report: ${type} in ${location}`,
-content:Buffer.from(JSON.stringify(json,null,2)).toString("base64"),
-sha:data.sha
-})
-});
-
-res.status(200).json({message:"Report submitted successfully"});
+res.json({message:"Report submitted"})
 }
---
+++ api/reports.js
@@ -0,0 +1,7 @@
+export default function handler(req,res){
+    global.reports = global.reports || []
+
+    res.json({
+        reports: global.reports
+    })
+}
