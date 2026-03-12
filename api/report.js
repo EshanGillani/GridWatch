ecad4e11efe580d3071722ccd302fbfd11b44a40
eastmohan <eastmohan22@gmail.com>
2026-03-12 15:03:00 -0400

    fixed reports.html

--- api/report.js
+++ api/report.js
@@ -1,21 +1,28 @@
-export default async function hander(req,res){
+export default async function handler(req,res){
+
     if(req.method !== "POST"){
         return res.status(405).json({error:"Method not allowed"})
     }
+    
+    const { zip, type, description } = req.body
 
+    const geo = await fetch(`https://api.zippopotam.us/us/${zip}`)
+    const geoData = await geo.json()
 
-const { zip, type, description } = req.body
+    const lat = parseFload(geoData.places[0].latitute)
+    const lng = parseFloat(geoData.places[0].longitute)
 
-const report={
-    zip,
-    type,
-    description,
-    time:Date.now()
-}
+    const report={
+        zip,
+        type,
+        description,
+        lat,
+        lng,
+        time:Date.now()
+    }
 
-global.reports = global.reports || []
+    global.reports = global.reports || []
+    global.reports.push(report)
 
-global.reports.push(report)
-
-res.json({message:"Report submitted"})
+    res.json({message:"Report submitted"})
 }
