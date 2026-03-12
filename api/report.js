f59a892b3cffff491c1a42fe00ff3f871c22de47
eastmohan <eastmohan22@gmail.com>
2026-03-12 15:12:12 -0400

    hopefully fixed reporting

--- api/report.js
+++ api/report.js
@@ -1,28 +1,28 @@
 export default async function handler(req,res){
 
-    if(req.method !== "POST"){
-        return res.status(405).json({error:"Method not allowed"})
-    }
-    
-    const { zip, type, description } = req.body
+if(req.method !== "POST"){
+return res.status(405).json({error:"Method not allowed"})
+}
 
-    const geo = await fetch(`https://api.zippopotam.us/us/${zip}`)
-    const geoData = await geo.json()
+const { zip, type, description } = req.body
 
-    const lat = parseFload(geoData.places[0].latitute)
-    const lng = parseFloat(geoData.places[0].longitute)
+const geo = await fetch(`https://api.zippopotam.us/us/${zip}`)
+const geoData = await geo.json()
 
-    const report={
-        zip,
-        type,
-        description,
-        lat,
-        lng,
-        time:Date.now()
-    }
+const lat = parseFloat(geoData.places[0].latitude)
+const lng = parseFloat(geoData.places[0].longitude)
 
-    global.reports = global.reports || []
-    global.reports.push(report)
+const report={
+zip,
+type,
+description,
+lat,
+lng,
+time:Date.now()
+}
 
-    res.json({message:"Report submitted"})
+global.reports = global.reports || []
+global.reports.push(report)
+
+res.json({message:"Report submitted"})
 }
--- api/reports.js
+++ api/reports.js
@@ -1,7 +1,8 @@
 export default function handler(req,res){
-    global.reports = global.reports || []
 
-    res.json({
-        reports: global.reports
-    })
+global.reports = global.reports || []
+
+res.json({
+reports: global.reports
+})
 }
